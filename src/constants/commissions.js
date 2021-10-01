92cb64fa92aafcaada2d35e0bcfe90ade184406d
Ramazan Chasygov <theliketurbo@gmail.com>
2021-10-01 12:34:21 +0300

    refac(Stacking): replace hook validation with formik

---
+++ src/constants/commissions.js
@@ -0,0 +1,1 @@
+export const STACKING = 3;
---
+++ src/constants/validationMessages.js
@@ -0,0 +1,2 @@
+export const NOT_ENOUGH = "Not enough tokens in your account";
+export const NOT_ENOUGH_CAUSE_COMMISSION = "Not enough to cover commission";
