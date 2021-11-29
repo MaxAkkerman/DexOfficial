1748546223cb608723963297e3b9585a1b30ebb1
Ramazan Chasygov <theliketurbo@gmail.com>
2021-11-29 19:38:17 +0300

    chore: add story for SearchInput

---
+++ src/components-v2/SearchInput/index.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SearchInput from "@/components-v2/SearchInput";
+
+export default {
+	component: SearchInput,
+	title: "Components/Search Input",
+};
+
+const Template = (args) => <SearchInput {...args} />;
+
+export const Default = Template.bind({});
