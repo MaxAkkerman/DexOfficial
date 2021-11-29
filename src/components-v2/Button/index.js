72d5ab8736b077754b4e1912f1eb088d6bed9e69
Ramazan Chasygov <theliketurbo@gmail.com>
2021-11-29 19:40:40 +0300

    chore: story for Button

---
+++ src/components-v2/Button/index.js
@@ -0,0 +1,29 @@
+import cls from "classnames";
+import PropTypes from "prop-types";
+import React from "react";
+
+export default function Button({children, className, disabled, onClick}) {
+	return (
+		<button
+			className={cls("btn", className, {
+				"btn--disabled": disabled,
+			})}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
+}
+
+Button.propTypes = {
+	children: PropTypes.node.isRequired,
+	className: PropTypes.string,
+	disabled: PropTypes.bool,
+	onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+	className: null,
+	disabled: false,
+	onClick: () => {},
+};
