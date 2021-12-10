573203a28b879ff9d836bfe39f25717a88dacfd3
MaxAkkerman <bagirokovanzor01@mail.ru>
2021-12-10 22:48:33 +0300

    fix login with different users

--- src/components/EnterPassword/EnterPassword.js
+++ src/components/EnterPassword/EnterPassword.js
@@ -4,6 +4,7 @@ import {useDispatch, useSelector} from "react-redux";
 import MainBlock from "../MainBlock/MainBlock";
 import "./EnterPassword.scss";
 import client, {
+    checkPubKey,
     getClientBalance,
     getClientKeys,
 } from "../../extensions/sdk_get/get";
@@ -46,159 +47,183 @@ function EnterPassword(props) {
         console.log("pin,step", pin, step)
         if (!pin) return;
         const curEmptyPin = pin.filter((item) => !item.value.length);
-        if (!curEmptyPin.length) {
-
-            let seedPhrasePassword = "";
-            pin.map(item => {
-                seedPhrasePassword += item.value.toString()
-            })
-            let esp = localStorage.getItem("esp");
-            let clientDataLS = JSON.parse(localStorage.getItem("clientData"));
-            let clientDataPreDeploy = JSON.parse(localStorage.getItem("clientDataPreDeploy"));
-
-            let decrypted = await decrypt(esp, seedPhrasePassword);
-
-            if (!decrypted.valid) {
-                setDecryptResult(false);
-                dispatch(
-                    setTips({
-                        message: `Something goes wrong - invalid password`,
-                        type: "error",
-                    }),
-                );
-                return
-            }else{
-                const clientKeys = await getClientKeys(decrypted.phrase);
-
-                setloadingUserDataIsWaiting(true);
-                setDecryptResult(true);
-
-                if (!clientDataLS && clientDataPreDeploy && clientDataPreDeploy.address) {
-                    saveLog({
-                        name: "login",
-                        clientAddress: clientDataPreDeploy.address,
-                        deployed: false,
-                        created_at: (Date.now() + 10800000) / 1000,
-                    }, "login")
-                    const dexClientAddress = clientDataPreDeploy.address;
-                    const dexClientBalance = await getClientBalance(dexClientAddress);
-                    dispatch(
-                        setClientData({
-                            status: false,
-                            dexclient: dexClientAddress,
-                            balance: dexClientBalance,
-                            public: clientKeys.public,
-                        }),
-                    );
-                    dispatch(setTransactionsList([]));
-
-                    dispatch(setSeedPassword(seedPhrasePassword));
-
-                    setloadingUserDataIsWaiting(false);
-                    dispatch(hideEnterSeedPhraseUnlock());
-                    history.push("/swap");
-                    return;
-                }else if(clientDataLS && clientDataLS.dexclient){
-                    saveLog({
-                        name: "login",
-                        clientAddress: clientDataLS.dexclient,
-                        deployed: true,
-                        created_at: (Date.now() + 10800000) / 1000,
-                    }, "login")
-                    await InitializeClient(clientKeys.public);
-                    dispatch(setSeedPassword(seedPhrasePassword));
-
-                    const receiveTokensData = JSON.parse(
-                        localStorage.getItem("setSubscribeReceiveTokens"),
-                    );
-                    if (receiveTokensData) {
-                        dispatch(setSubscribeReceiveTokens(receiveTokensData));
-                    }
-
-                }else{
-                    dispatch(
-                        setTips({
-                            message: `Something goes wrong, please deploy new client or enter using seed phrase`,
-                            type: "error",
-                        }),
-                    );
-                }
-            }
-            setloadingUserDataIsWaiting(false);
-            dispatch(hideEnterSeedPhraseUnlock());
-            history.push("/swap");
-        } else {
+        console.log("curEmptyPin",curEmptyPin)
+        if (curEmptyPin.length) {
             dispatch(
                 setTips({
                     message: `Wrong PIN, please try again`,
                     type: "error",
                 }),
             );
+            return
         }
-    }
 
-    function passwordChange(event) {
-        let password = event.target.value;
-        if (password.length > 0) setValidPassword(true);
-        else setDecryptResult(null);
-        setSeedPhrasePassword(password);
-    }
+        let seedPhrasePassword = "";
+        pin.map(item => {
+            seedPhrasePassword += item.value.toString()
+        })
+        let esp = localStorage.getItem("esp");
+        let clientDataLS = JSON.parse(localStorage.getItem("clientData"));
+        let clientDataPreDeploy = JSON.parse(localStorage.getItem("clientDataPreDeploy"));
+        console.log("esp, seedPhrasePassword", esp, seedPhrasePassword)
+        let decrypted = await decrypt(esp, seedPhrasePassword);
 
-    function handleLogOut() {
-        localStorage.removeItem("esp");
-        localStorage.removeItem("setSubscribeReceiveTokens");
-        window.location.reload();
-    }
+        const clientKeys = await getClientKeys(decrypted.phrase);
 
-    const CssTextField = styled(TextField)({
-        "& .MuiOutlinedInput-input": {
-            "&.Mui-disabled": {
-                color: "var(--text-color)",
-                "-webkit-text-fill-color": "unset",
-            },
-            color: "var(--text-color)",
-        },
-        "& .Mui-disabled": {
-            color: "var(--text-color)",
-        },
-    });
+        let clientStatus = await checkPubKey(clientKeys.public);
 
-    const [pinsConfirmed, setPinsConfirmed] = useState(true);
+        let clientExists = clientStatus.status && decrypted.valid && (clientStatus.dexclient !== "0:0000000000000000000000000000000000000000000000000000000000000000")
+        console.log("clientExists",clientStatus.status , decrypted.valid , clientStatus.dexclient )
+        let preDeployedClientExists = clientDataPreDeploy && clientDataPreDeploy.address && clientDataPreDeploy.esp
+        console.log("preDeployedClientExists",clientDataPreDeploy)
 
-    function handleCheckPin(pinArr, step) {
-        // console.log("pinArr", pinArr)
-        dispatch(setPin(pinArr));
+        //
+        // if (!decrypted.valid) {
+        //     setDecryptResult(false);
+        //     // dispatch(
+        //     //     setTips({
+        //     //         message: `Something goes wrong - invalid password`,
+        //     //         type: "error",
+        //     //     }),
+        //     // );
+        //     // return
+        // }
 
-    }
 
-    return (
-        <>
-            {loadingUserDataIsWaiting ? (
-                <WaitingPopup
-                    hide={true}
-                    title={"Connecting to blockchain"}
-                    text={`Loading user data...`}
-                />
-            ) : (
-                <div className="select-wrapper">
-                    <PinPopup
-                        title={"Enter your Pin"}
-                        nextStep={"step3"}
-                        prevStep={"step1"}
-                        handleLogOut={() => handleLogOut()}
-                        btnText={"Log in"}
-                        pinCorrect={pinsConfirmed}
-                        handleClickBack={null}
-                        handleClose={null}
-                        handleClickNext={(pin) => handleLogIn(pin)}
-                        handleCheckPin={(pin, step, completed) => handleCheckPin(pin, step,completed)}
-                    />
-                </div>
-            )
+        // setloadingUserDataIsWaiting(true);
+        // setDecryptResult(true);
+        // console.log("clientDataLS",clientDataLS,clientDataPreDeploy,clientDataPreDeploy.address)
+
+
+
+
+        if (clientExists) {
+            setloadingUserDataIsWaiting(true);
+            setDecryptResult(true);
+            console.log("clientDataLS exists", clientDataLS, "clientKeys", clientKeys)
+            saveLog({
+                name: "login",
+                clientAddress: clientStatus.dexclient,
+                deployed: true,
+                created_at: (Date.now() + 10800000) / 1000,
+            }, "login")
+            await InitializeClient(clientKeys.public);
+            dispatch(setSeedPassword(seedPhrasePassword));
+            console.log("i am after init client")
+            const receiveTokensData = JSON.parse(
+                localStorage.getItem("setSubscribeReceiveTokens"),
+            );
+            if (receiveTokensData) {
+                dispatch(setSubscribeReceiveTokens(receiveTokensData));
             }
+            setloadingUserDataIsWaiting(false);
+            dispatch(hideEnterSeedPhraseUnlock());
+            history.push("/swap");
 
-        </>
-    )
+        } else if (!clientExists && preDeployedClientExists) {
+            console.log("clientDataPreDeploy exists", clientDataLS)
+            setloadingUserDataIsWaiting(true);
+            setDecryptResult(true);
+            saveLog({
+                name: "login",
+                clientAddress: clientDataPreDeploy.address,
+                deployed: false,
+                created_at: (Date.now() + 10800000) / 1000,
+            }, "login")
+            const dexClientAddress = clientDataPreDeploy.address;
+            const dexClientBalance = await getClientBalance(dexClientAddress);
+            dispatch(
+                setClientData({
+                    status: false,
+                    dexclient: dexClientAddress,
+                    balance: dexClientBalance,
+                    public: clientKeys.public,
+                }),
+            );
+            dispatch(setTransactionsList([]));
+
+            dispatch(setSeedPassword(seedPhrasePassword));
+
+            setloadingUserDataIsWaiting(false);
+            dispatch(hideEnterSeedPhraseUnlock());
+            history.push("/swap");
+
+
+        } else {
+            dispatch(
+                setTips({
+                    message: `Something goes wrong, please deploy new client or enter using seed phrase`,
+                    type: "error",
+                }),
+            );
+        }
+
+
+
+}
+
+function passwordChange(event) {
+    let password = event.target.value;
+    if (password.length > 0) setValidPassword(true);
+    else setDecryptResult(null);
+    setSeedPhrasePassword(password);
+}
+
+function handleLogOut() {
+    localStorage.removeItem("esp");
+    localStorage.removeItem("setSubscribeReceiveTokens");
+    window.location.reload();
+}
+
+const CssTextField = styled(TextField)({
+    "& .MuiOutlinedInput-input": {
+        "&.Mui-disabled": {
+            color: "var(--text-color)",
+            "-webkit-text-fill-color": "unset",
+        },
+        color: "var(--text-color)",
+    },
+    "& .Mui-disabled": {
+        color: "var(--text-color)",
+    },
+});
+
+const [pinsConfirmed, setPinsConfirmed] = useState(true);
+
+function handleCheckPin(pinArr, step) {
+    // console.log("pinArr", pinArr)
+    dispatch(setPin(pinArr));
+
+}
+
+return (
+    <>
+        {loadingUserDataIsWaiting ? (
+            <WaitingPopup
+                hide={true}
+                title={"Connecting to blockchain"}
+                text={`Loading user data...`}
+            />
+        ) : (
+            <div className="select-wrapper">
+                <PinPopup
+                    title={"Enter your Pin"}
+                    nextStep={"step3"}
+                    prevStep={"step1"}
+                    handleLogOut={() => handleLogOut()}
+                    btnText={"Log in"}
+                    pinCorrect={pinsConfirmed}
+                    handleClickBack={null}
+                    handleClose={null}
+                    handleClickNext={(pin) => handleLogIn(pin)}
+                    handleCheckPin={(pin, step, completed) => handleCheckPin(pin, step, completed)}
+                />
+            </div>
+        )
+        }
+
+    </>
+)
 }
 
 export default EnterPassword;
