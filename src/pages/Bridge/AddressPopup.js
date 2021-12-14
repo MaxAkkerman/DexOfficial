d716d95414c8f3127fa2d5e247ea0b8f5be5f360
Zarema <zarema@MacBook-Pro-Zarema.local>
2021-12-14 17:18:02 +0300

    stable preview for logic implementation

---
+++ src/pages/Bridge/AddressPopup.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {handleCutAddress} from "../../reactUtils/reactUtils";
+
+function AddressPopup(props) {
+    return (
+
+        <div
+            className="bridge_address_wrapper"
+
+        >
+            <div className="bridge_address_img">
+                <img
+                    className="send_token_img"
+                    src={props.netIcon}
+                    alt={"arrow"}
+                />&nbsp;
+
+            </div>
+            {handleCutAddress(props.address)}
+        </div>
+
+
+    );
+}
+
+export default AddressPopup;
---
+++ src/pages/Bridge/AssetsListBridge.js
@@ -0,0 +1,45 @@
+import MainBlock from "../../components/MainBlock/MainBlock";
+import CloseBtn from "../../components/CloseBtn/CloseBtn";
+import SearchInput from "../../components/SearchInput/SearchInput";
+import AssetsList from "../../components/AssetsList/AssetsList";
+import React, {useState} from "react";
+
+function AssetsListBridge(props) {
+    const [filter,setFilter] = useState("")
+    function handleSearch(searchText){
+        setFilter(searchText)
+    }
+
+    return (
+
+<div className="select-wrapper" onClick={(e) => props.handleClose(e)}>
+    <MainBlock
+        title={"Select a token"}
+        button={<CloseBtn func={(e) => props.handleClose(e)} />}
+        content={
+            <>
+                <SearchInput func={(e) => handleSearch(e)} />
+
+                <AssetsList
+                    handleClickNFT={null}
+                    handleClickToken={(item) => props.handleSetToken(item)}
+                    TokenAssetsArray={props.assets.filter(
+                        (item) =>
+                            item.name
+                                .toLowerCase()
+                                .includes(filter.toLowerCase())
+
+                    )}
+                    NFTassetsArray={null}
+                    orderAssetsArray={null}
+                    showItBeShown={false}
+                    filter={null}
+                />
+            </>
+        }
+    />
+</div>
+    );
+}
+
+export default AssetsListBridge;
