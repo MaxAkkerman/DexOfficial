31f882bf711c2141b17ea55a0169df31993a0b3b
Ramazan Chasygov <theliketurbo@gmail.com>
2021-12-15 13:40:42 +0300

    chore: add storybook decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import "@/index.scss";
+import {StyledEngineProvider} from "@mui/material/styles";
 
 export const parameters = {
 	actions: {argTypesRegex: "^on[A-Z].*"},
@@ -9,3 +10,11 @@ export const parameters = {
 		},
 	},
 };
+
+export const decorators = [
+	(Story) => (
+		<StyledEngineProvider injectFirst>
+			<Story />
+		</StyledEngineProvider>
+	),
+];
