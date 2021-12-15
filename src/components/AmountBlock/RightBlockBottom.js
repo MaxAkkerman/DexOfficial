9a21c37d339a19cc78f944d497eb8cb56ab64105
mzoterov <mzoterov@yandex.ru>
2021-12-15 15:19:02 +0300

    1. Header Items 2. Prettier 3. Fix enter seedphrase

--- src/components/AmountBlock/RightBlockBottom.js
+++ src/components/AmountBlock/RightBlockBottom.js
@@ -1,49 +1,66 @@
-import React from "react";
-import {useDispatch, useSelector} from "react-redux";
-import SetTokenBlock from "./SetTokenBlock";
-import SelectTokenMenu from "./SelectTokenMenu";
-import {useHistory} from "react-router-dom";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import SelectTokenMenu from './SelectTokenMenu';
+import SetTokenBlock from './SetTokenBlock';
 
 function RightBlockBottom(props) {
-	const history = useHistory();
-	const tokenSetted = useSelector((state) => state.walletSeedReducer.tokenSetted);
-	const currentTokenForSend = useSelector((state) => state.walletSeedReducer.currentTokenForSend);
-	const inputNFTdisabled = useSelector((state) => state.walletSeedReducer.inputNFTdisabled);
+  const history = useHistory();
+  const tokenSetted = useSelector(
+    (state) => state.walletSeedReducer.tokenSetted,
+  );
+  const currentTokenForSend = useSelector(
+    (state) => state.walletSeedReducer.currentTokenForSend,
+  );
+  const inputNFTdisabled = useSelector(
+    (state) => state.walletSeedReducer.inputNFTdisabled,
+  );
 
-	function handleTouchTokenModal() {
-		history.push("/wallet/send/send-modal");
-	}
+  function handleTouchTokenModal() {
+    history.push('/wallet/send/send-modal');
+  }
 
-	function handleTouchTokenModal2() {
-		history.push("/wallet/send/send-modal");
-	}
+  function handleTouchTokenModal2() {
+    history.push('/wallet/send/send-modal');
+  }
 
-	return (
-		<>
-			{(props.tokenSetted ? props.tokenSetted : tokenSetted) ? (
-				<>
-					<div className="send_set_token_wrap column">
-						{inputNFTdisabled === "disabled" ? (
-							<div style={{width: "52px"}} />
-						) : (
-							props.enableMax
-						)}
-						<SetTokenBlock
-							handleTouchTokenModal={props.showAssetsList ? ()=>props.showAssetsList() : () => handleTouchTokenModal()}
-							// img={TON}
-							currentToken={props.curAsset ? props.curAsset : currentTokenForSend}
-						/>
-					</div>
-				</>
-			) : (
-				<div className="send_select_wrap">
-					<SelectTokenMenu
-						handleTouchTokenModal2={props.showAssetsList ? ()=>props.showAssetsList() : () => handleTouchTokenModal2()}
-					/>
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      {(props.tokenSetted ? props.tokenSetted : tokenSetted) ? (
+        <>
+          <div className="send_set_token_wrap column">
+            {inputNFTdisabled === 'disabled' ? (
+              <div style={{ width: '52px' }} />
+            ) : (
+              props.enableMax
+            )}
+            <SetTokenBlock
+              handleTouchTokenModal={
+                props.showAssetsList
+                  ? () => props.showAssetsList()
+                  : () => handleTouchTokenModal()
+              }
+              // img={TON}
+              currentToken={
+                props.curAsset ? props.curAsset : currentTokenForSend
+              }
+            />
+          </div>
+        </>
+      ) : (
+        <div className="send_select_wrap">
+          <SelectTokenMenu
+            handleTouchTokenModal2={
+              props.showAssetsList
+                ? () => props.showAssetsList()
+                : () => handleTouchTokenModal2()
+            }
+          />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default RightBlockBottom;
--- src/pages/Bridge/AssetsListBridge.js
+++ src/pages/Bridge/AssetsListBridge.js
@@ -1,45 +1,41 @@
-import MainBlock from "../../components/MainBlock/MainBlock";
-import CloseBtn from "../../components/CloseBtn/CloseBtn";
-import SearchInput from "../../components/SearchInput/SearchInput";
-import AssetsList from "../../components/AssetsList/AssetsList";
-import React, {useState} from "react";
+import React, { useState } from 'react';
+
+import AssetsList from '../../components/AssetsList/AssetsList';
+import CloseBtn from '../../components/CloseBtn/CloseBtn';
+import MainBlock from '../../components/MainBlock/MainBlock';
+import SearchInput from '../../components/SearchInput/SearchInput';
 
 function AssetsListBridge(props) {
-    const [filter,setFilter] = useState("")
-    function handleSearch(searchText){
-        setFilter(searchText)
-    }
+  const [filter, setFilter] = useState('');
+  function handleSearch(searchText) {
+    setFilter(searchText);
+  }
 
-    return (
-
-<div className="select-wrapper" onClick={(e) => props.handleClose(e)}>
-    <MainBlock
-        title={"Select a token"}
+  return (
+    <div className="select-wrapper" onClick={(e) => props.handleClose(e)}>
+      <MainBlock
+        title={'Select a token'}
         button={<CloseBtn func={(e) => props.handleClose(e)} />}
         content={
-            <>
-                <SearchInput func={(e) => handleSearch(e)} />
+          <>
+            <SearchInput func={(e) => handleSearch(e)} />
 
-                <AssetsList
-                    handleClickNFT={null}
-                    handleClickToken={(item) => props.handleSetToken(item)}
-                    TokenAssetsArray={props.assets.filter(
-                        (item) =>
-                            item.name
-                                .toLowerCase()
-                                .includes(filter.toLowerCase())
-
-                    )}
-                    NFTassetsArray={null}
-                    orderAssetsArray={null}
-                    showItBeShown={false}
-                    filter={null}
-                />
-            </>
+            <AssetsList
+              handleClickNFT={null}
+              handleClickToken={(item) => props.handleSetToken(item)}
+              TokenAssetsArray={props.assets.filter((item) =>
+                item.name.toLowerCase().includes(filter.toLowerCase()),
+              )}
+              NFTassetsArray={null}
+              orderAssetsArray={null}
+              showItBeShown={false}
+              filter={null}
+            />
+          </>
         }
-    />
-</div>
-    );
+      />
+    </div>
+  );
 }
 
 export default AssetsListBridge;
