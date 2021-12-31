3ea5b2b2f2761122a779ecac811dc8c833b2640c
Ramazan Chasygov <theliketurbo@gmail.com>
2021-12-31 17:05:53 +0300

    fix: add footer style

--- src/components-v2/LimitOrderCancelPopup/index.js
+++ src/components-v2/LimitOrderCancelPopup/index.js
@@ -178,7 +178,7 @@ export default function LimitOrderCancelPopup() {
         footer={
           <div className="mainblock-footer">
             <div className="mainblock-footer-wrap">
-              <div className="swap-confirm-wrap">
+              <div className={classes['swap-confirm-wrap']}>
                 <p className="mainblock-footer-value">
                   {toPrice} {toToken.symbol}
                 </p>
